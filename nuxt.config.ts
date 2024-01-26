d78b24727209c02d6458f5e7973f070eb1e7c489
Sergey Lisovskiy <lisovskiyserj@gmail.com>
2024-01-26 15:04:41 +0400

    feat: meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+const title = 'SORA Card | Value Freedom',
+  description = 'Access the new era of interoperable finance, all in the palm of your hands.',
+  keyWords =
+    'SORA, Polkaswap, Decentralization, DeFi, Future, Sortition, Metaverse, TradFi, Card, Crypto',
+  ogImage = '/og.jpg'
+
 export default defineNuxtConfig({
   srcDir: 'src/',
   devtools: { enabled: false },
@@ -7,6 +12,102 @@ export default defineNuxtConfig({
   content: {
     markdown: {
       anchorLinks: false,
+    },
+  },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'en',
+      },
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: title,
+      link: [
+        {
+          rel: 'icon',
+          href: '/favicon.ico',
+          sizes: '32x32',
+        },
+        {
+          rel: 'icon',
+          href: '/icon.svg',
+          type: 'image/svg+xml',
+        },
+        {
+          rel: 'apple-touch-icon',
+          href: '/apple-touch-icon.png',
+        },
+      ],
+      meta: [
+        {
+          name: 'description',
+          content: description,
+        },
+        {
+          name: 'keywords',
+          content: keyWords,
+        },
+        {
+          name: 'twitter:title',
+          content: title,
+        },
+        {
+          name: 'twitter:description',
+          content: description,
+        },
+        {
+          name: 'twitter:image',
+          content: ogImage,
+        },
+        {
+          name: 'twitter:image:alt',
+          content: title,
+        },
+        {
+          name: 'twitter:card',
+          content: 'summary_large_image',
+        },
+        {
+          name: 'twitter:site',
+          content: '@sora_xor',
+        },
+        {
+          property: 'og:title',
+          content: title,
+        },
+        {
+          property: 'og:description',
+          content: description,
+        },
+        {
+          property: 'og:image',
+          content: ogImage,
+        },
+        {
+          property: 'og:image:secure_url',
+          content: ogImage,
+        },
+        {
+          property: 'og:image:alt',
+          content: title,
+        },
+        {
+          property: 'og:image:width',
+          content: '1200',
+        },
+        {
+          property: 'og:image:height',
+          content: '630',
+        },
+        {
+          property: 'og:url',
+          content: 'https://soracard.com',
+        },
+        {
+          property: 'og:type',
+          content: 'website',
+        },
+      ],
     },
   },
 })
