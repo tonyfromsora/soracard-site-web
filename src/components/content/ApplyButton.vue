280437737f190b659d0399708231df6675b43635
Sergey Lisovskiy <lisovskiyserj@gmail.com>
2024-04-15 22:37:41 +0400

    feat: blog `ApplyButton`

---
+++ src/components/content/ApplyButton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const isModalOpen = useApplyModalState()
+</script>
+
+<template>
+  <button class="hover-trigger accent" @click="isModalOpen = true">
+    <span class="hover-underline">
+      <ContentSlot :use="$slots.default" unwrap="p" />
+    </span>
+  </button>
+</template>
