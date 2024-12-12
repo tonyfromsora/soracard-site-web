5b67e1ab7dfa2b02b0fc62862d7100e731be6637
tarmovannas <tarmo@tarmovannas.com>
2024-12-12 14:57:08 +0000

    add-redirects

---
+++ src/pages/es/consumer-crypto/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 class="text-3xl w text-center pt-3xl pb-xl" data-aos="fade-up">Esta página se redirigirá automáticamente en {{ timeLeft }}s</h1>
+</template>
+
+<script setup>
+const timeLeft = ref(1);
+
+onMounted(() => {
+setInterval(async () => {
+  if (timeLeft.value === 1) {
+    await navigateTo("/es/terms", {
+      external: false,
+    });
+  }
+
+  timeLeft.value = timeLeft.value - 1;
+}, 1000);
+});
+</script>
